971a921f0d98debb7bc85249a4409d33013a3918
Henrik L. Sønder <soender@gmail.com>
2024-09-18 17:20:29 +0200

    wip: enable tests in gherkin

---
+++ src/features/Example.feature
@@ -0,0 +1,6 @@
+Feature: Example feature
+
+  Scenario: Example scenario
+    Given I have a configured cucumber environment
+    When I run the tests
+    Then I should see the results
---
+++ src/features/step_definitions/Example.steps.ts
@@ -0,0 +1,16 @@
+import { Given, When, Then } from '@cucumber/cucumber';
+
+Given('I have a configured cucumber environment', function () {
+  // Write code here that turns the phrase above into concrete actions
+  console.log('Configured cucumber environment');
+});
+
+When('I run the tests', function () {
+  // Write code here that turns the phrase above into concrete actions
+  console.log('Running tests');
+});
+
+Then('I should see the results', function () {
+  // Write code here that turns the phrase above into concrete actions
+  console.log('Seeing results');
+});
