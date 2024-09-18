f584c9b206ab2db6eba1c6395c84bec9a0aed245
fullStackOasis <fullstackdev@fullstackoasis.com>
2024-09-18 13:32:11 -0400

    feat: raise when our hole cards are A K Q or J

--- src/Player.spec.ts
+++ src/Player.spec.ts
@@ -12,7 +12,7 @@ Player raises a bet:
 	current_buy_in - players[in_action][bet] + minimum_raise
 */
 describe('Player', () => {
-  test('should always make a call', () => {
+  test('should always make a call when there are no hole cards', () => {
     var player = new Player();
     var betCallbackResult = 0;
     var betCallback = (bet: number) => { betCallbackResult = bet; };
@@ -47,4 +47,107 @@ describe('Player', () => {
     // Assert
     expect(betCallbackResult).toBe(320);
   });
+
+
+  test('raises with minimum raise', () => {
+    var player = new Player();
+    var betCallbackResult = 0;
+    var betCallback = (bet: number) => { betCallbackResult = bet; };
+    var gameState = new GameStateImpl();
+    gameState.minimum_raise = 240;
+    gameState.players = 
+      [
+        {
+          name: "Player 1",
+          stack: 1000,
+          status: "active",
+          bet: 0,
+          hole_cards: [],
+          version: "Version name 1",
+          id: 0
+        },
+        {
+          name: "Player 2",
+          stack: 1000,
+          status: "active",
+          bet: 0,
+          hole_cards: [
+            {
+              // Rank of the card. Possible values are 
+              // numbers 2-10 and J,Q,K,A 
+              "rank": "6",                    
+              // Suit of the card. Possible values are: 
+              // clubs,spades,hearts,diamonds 
+              "suit": "hearts"                
+            },
+            {
+              "rank": "A",
+              "suit": "spades"
+            }
+          ],
+          version: "Version name 2",
+          id: 1
+        }
+      ]
+    gameState.current_buy_in = 320;
+    gameState.in_action = 1;
+
+    // Act
+    player.betRequest(gameState, betCallback)
+    
+    // Assert
+    expect(betCallbackResult).toBe(320 + 240);
+  });
+
+
+  test('raises with minimum raise', () => {
+    var player = new Player();
+    var betCallbackResult = 0;
+    var betCallback = (bet: number) => { betCallbackResult = bet; };
+    var gameState = new GameStateImpl();
+    gameState.minimum_raise = 240;
+    gameState.players = 
+      [
+        {
+          name: "Player 1",
+          stack: 1000,
+          status: "active",
+          bet: 0,
+          hole_cards: [],
+          version: "Version name 1",
+          id: 0
+        },
+        {
+          name: "Player 2",
+          stack: 1000,
+          status: "active",
+          bet: 0,
+          hole_cards: [
+            {
+              // Rank of the card. Possible values are 
+              // numbers 2-10 and J,Q,K,A 
+              "rank": "6",
+              // Suit of the card. Possible values are: 
+              // clubs,spades,hearts,diamonds 
+              "suit": "hearts"
+            },
+            {
+              "rank": "J",
+              "suit": "spades"
+            }
+          ],
+          version: "Version name 2",
+          id: 1
+        }
+      ]
+    gameState.current_buy_in = 320;
+    gameState.in_action = 1;
+
+    // Act
+    player.betRequest(gameState, betCallback)
+
+    // Assert
+    expect(betCallbackResult).toBe(320 + 240);
+  });
+
 });
--- src/Player.ts
+++ src/Player.ts
@@ -1,14 +1,36 @@
-import { GameState } from "./GameState";
+import { Card, GameState } from "./GameState";
 
 export class Player {
   public betRequest(gameState: GameState, betCallback: (bet: number) => void): void {
     console.log(`Show gameState: ${JSON.stringify(gameState)}`);
-    var nextBet = this.call(gameState);
+    var nextBet;
+    if (this.weHaveAceKQJ(gameState.players[gameState.in_action].hole_cards)) {
+      nextBet = this.raise(gameState);
+    } else {
+      nextBet = this.call(gameState);
+    }
     betCallback(nextBet);
   }
 
   private call(gameState: GameState) {
     return gameState.current_buy_in - gameState.players[gameState.in_action].bet;
+  }
+
+  // raise if we have an ace or K, Q, or J?
+  private raise(gameState: GameState) {
+    return gameState.current_buy_in - gameState.players[gameState.in_action].bet +
+      gameState.minimum_raise;
+  }
+
+  private weHaveAceKQJ(holeCards: Card[]) {
+    let n = holeCards.length;
+    for (var index = 0; index < n; index++) {
+      const rank = holeCards[index].rank;
+      if (rank == 'A' || rank == 'K' || rank == 'Q' || rank == 'J' ) {
+        return true;
+      }
+    }
+    return false;
   }
 
   public showdown(gameState: GameState): void {
