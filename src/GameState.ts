b9904af403282ec85452207a07aae10cc163a241
Henrik L. Sønder <soender@gmail.com>
2024-09-18 19:19:00 +0200

    refactor: add missing fields to gamestate

--- src/GameState.ts
+++ src/GameState.ts
@@ -14,18 +14,19 @@ interface Player {
 }
 
 interface GameState {
-  players: Player[];
   tournament_id: string;
   game_id: string;
   round: number;
   bet_index: number;
   small_blind: number;
-  orbits: number;
-  dealer: number;
-  community_cards: Card[];
   current_buy_in: number;
   pot: number;
+  minimum_raise: number;
+  dealer: number;
+  orbits: number;
   in_action: number;
+  players: Player[];
+  community_cards: Card[];
 }
 
 export { Card, Player, GameState };
--- src/GameStateImpl.ts
+++ src/GameStateImpl.ts
@@ -1,50 +1,65 @@
 import { Card, Player, GameState } from './GameState';
 
 class GameStateImpl implements GameState {
-  players: Player[];
   tournament_id: string;
   game_id: string;
   round: number;
   bet_index: number;
   small_blind: number;
-  orbits: number;
-  dealer: number;
-  community_cards: Card[];
   current_buy_in: number;
   pot: number;
+  minimum_raise: number;
+  dealer: number;
+  orbits: number;
   in_action: number;
+  players: Player[];
+  community_cards: Card[];
 
   // Overloaded constructor signatures
   constructor();
-  constructor(players: Player[], tournament_id: string, game_id: string, round: number, bet_index: number, small_blind: number, orbits: number, dealer: number, community_cards: Card[], current_buy_in: number, pot: number, in_action: number);
-
+  constructor(tournament_id: string,
+    game_id: string,
+    round: number,
+    bet_index: number,
+    small_blind: number,
+    current_buy_in: number,
+    pot: number,
+    minimum_raise: number,
+    dealer: number,
+    orbits: number,
+    in_action: number,
+    players: Player[],
+    community_cards: Card[])
   // Single constructor implementation
   constructor(
-    players: Player[] = [],
     tournament_id: string = '',
     game_id: string = '',
     round: number = 0,
     bet_index: number = 0,
     small_blind: number = 0,
-    orbits: number = 0,
-    dealer: number = 0,
-    community_cards: Card[] = [],
     current_buy_in: number = 0,
     pot: number = 0,
-    in_action: number = 0
+    minimum_raise: number = 0,
+    dealer: number = 0,
+    orbits: number = 0,
+    in_action: number = 0,
+    players: Player[] = [],
+    community_cards: Card[] = [],
+
   ) {
-    this.players = players;
     this.tournament_id = tournament_id;
     this.game_id = game_id;
     this.round = round;
     this.bet_index = bet_index;
     this.small_blind = small_blind;
-    this.orbits = orbits;
-    this.dealer = dealer;
-    this.community_cards = community_cards;
     this.current_buy_in = current_buy_in;
     this.pot = pot;
+    this.minimum_raise = minimum_raise;
+    this.dealer = dealer;
+    this.orbits = orbits;
     this.in_action = in_action;
+    this.players = players;
+    this.community_cards = community_cards;
   }
 }
 
